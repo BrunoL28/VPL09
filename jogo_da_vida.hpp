26e5c8639b0f46aa7b772dfae7397577e64401f4
Bruno Lima Soares <112128273+BrunoL28@users.noreply.github.com>
2022-11-20 13:38:43 -0300

    implementado class ExcecaoCelulaInvalida
    Implementação da classe responsável pelo tratamento de erros, com um construtor e uma função what() sobrescrita.

--- jogo_da_vida.hpp
+++ jogo_da_vida.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <vector>
+#include <string>
+#include <exception>
 
 // Implementa o Jogo da Vida de John Conway.
 // https://pt.wikipedia.org/wiki/Jogo_da_vida
+
 class JogoDaVida {
+ 
  public:
+ 
   // Cria um jogo cujo torus tem l linhas e c colunas.
   // Todas as células são inicializadas como mortas.
+ 
   JogoDaVida(int l, int c);
 
   // Retorna o número de linhas do torus.
@@ -21,6 +27,7 @@ class JogoDaVida {
 
   // Alteram o estado célula [i, j].
   void Matar(int i, int j);
+ 
   void Reviver(int i, int j);
 
   // Executa a próxima iteração do jogo da vida.
@@ -28,10 +35,29 @@ class JogoDaVida {
 
   // Executa n iterações do jogo da vida.
   void Executar(int n);
+ 
  private:
+ 
   // Conta o número de vizinhas vivas da célula [x, y].
   int NumeroDeVizinhasVivas(int x, int y);
 
   // Armazena o estado das células.
    std::vector<std::vector<bool>> vivas_;  
+ 
+};
+
+class ExcecaoCelulaInvalida : public std::exception {
+
+   private:
+
+      int _m_error;  //linha do erro
+      int _n_error;   //coluna do erro
+      std::string _msg_error;
+
+   public:
+
+      ExcecaoCelulaInvalida(int m, int n);
+
+      const char * what() const noexcept override;
+
 };
